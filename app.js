432508a16439cb556e2ed58fad39c68c157ded20
Kaio Luiz Ferreira <53657502+kaio358@users.noreply.github.com>
2023-05-09 19:33:20 -0300

    aprendendo localStorage
    Estou tentando fazer uma aplicação para salvar um item novo, mas quando e atualizado ele meio que começa a rescrever por cima ,estou pensando em fazer uma chave que salva o ultimo  id mencionado assim o usuario poderá continuar a escrever de onde parou

--- app.js
+++ app.js
@@ -10,37 +10,53 @@ const form_control = document.querySelector(".form-control")
 const btn_submit = document.querySelector(".btn")
 //contador 
 let cont = 0 
-// itens a serem salvos 
-let itens = []
+
+
 const fragment = document.createDocumentFragment()
 // ul -> lista
 const transactions = document.querySelector("#transactions") 
 
+// itens a serem salvos 
+let itens = {}
+if (localStorage.getItem("itens")) {
+  itens = JSON.parse(localStorage.getItem("itens"))
+}
+
 btn_submit.addEventListener('click',(event)=>{
     event.preventDefault();
     cont++;
-  
-    itens.push({"id":cont,"nome": text.value,"valor":  amount.value })
+    
+    itens[ cont] = {"id":cont,"nome": text.value,"valor":  amount.value }
     text.value = ""
     amount.value = ""
+    localStorage.setItem("itens",JSON.stringify(itens))
+    
+    // let parametro = localStorage.getItem(itens[1])
+    // console.log(parametro);
+    // let dados = JSON.parse(parametro)
+    // console.log(dados);
     criadorDeTags()
+
  
 })
+
 
 
 function criadorDeTags() {
     
     const li = document.createElement("li")
-    
-    if(itens[cont-1]["valor"]>0){
+    console.log(itens[cont]["valor"]);
+    if(itens[cont]["valor"]>0){
         li.classList.add("plus")
-        li.innerHTML = itens[cont-1]["nome"] + `<span>${itens[cont-1]["valor"]}</span> <button class="delete-btn">x</button>`
+        li.innerHTML = itens[cont]["nome"] + `<span>${itens[cont]["valor"]}</span> <button class="delete-btn">x</button>`
     }else{
         li.classList.add("minus")
-        li.innerHTML = itens[cont-1]["nome"] + `<span>${itens[cont-1]["valor"]}</span> <button class="delete-btn">x</button>`
+        li.innerHTML = itens[cont]["nome"] + `<span>${itens[cont]["valor"]}</span> <button class="delete-btn">x</button>`
     }
     fragment.appendChild(li)
-    
+    transactions.appendChild(fragment)
+ 
 }
+
 
 
